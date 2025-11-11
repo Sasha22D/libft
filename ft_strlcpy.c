796ffcdbc438930c597225308f418a5290347ba6
Sasha Daniel <sadaniel@f1r10s7.clusters.42paris.fr>
2025-11-11 14:16:10 +0100

    correc strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: sadaniel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 15:14:48 by sadaniel          #+#    #+#             */
-/*   Updated: 2025/11/10 15:41:35 by sadaniel         ###   ########.fr       */
+/*   Updated: 2025/11/11 14:15:45 by sadaniel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -14,16 +14,19 @@
 size_t	ft_strlcpy(char *dst, const char *src, size_t siz)
 {
 	size_t	i;
+	size_t	src_len;
+	size_t	space_left;
 
+	space_left = size - 1;
+	src_len = ft_strlen(src);
 	i = 0;
 	if (siz == 0)
-		return (ft_strlen(src));
-	while (i < siz)
+		return (src_len);
+	while (src[i] && i < space_left)
 	{
 		dst[i] = src[i];
 		i++;
 	}
-	if (siz > 0)
-		dst[i] = '\0';
-	return (ft_strlen(src));
+	dst[i] = '\0';
+	return (src_len);
 }
