101dc67e4d0330694600bc4e99125f7388f9ce1b
Sasha Daniel <sadaniel@f4r13s14.clusters.42paris.fr>
2025-11-13 16:34:15 +0100

    ft_strtrim + proto

---
+++ ft_strtrim.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sadaniel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/13 13:42:22 by sadaniel          #+#    #+#             */
+/*   Updated: 2025/11/13 16:33:15 by sadaniel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+static int	ft_isinset(char c, char const *set)
+{
+	while (*set)
+	{
+		if (c == *set)
+			return (1);
+		set++;
+	}
+	return (0);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	start;
+	size_t	end;
+	char	*str;
+	char	*p;
+
+	start = 0;
+	if (s1[0] == '\0')
+	{
+		str = malloc(1);
+		str[0] = '\0';
+		return (str);
+	}
+	end = ft_strlen(s1) - 1;
+	while (ft_isinset(s1[start], set) == 1)
+		start++;
+	while (end > start && ft_isinset(s1[end], set) == 1)
+		end--;
+	str = malloc((end - start + 1 + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	p = str;
+	while (start <= end)
+		*p++ = s1[start++];
+	*p = '\0';
+	return (str);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: sadaniel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/07 12:46:55 by sadaniel          #+#    #+#             */
-/*   Updated: 2025/11/13 13:11:33 by sadaniel         ###   ########.fr       */
+/*   Updated: 2025/11/13 16:34:03 by sadaniel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBFT_H
@@ -40,5 +40,6 @@ void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
 
 #endif
